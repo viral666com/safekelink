fda7f7c648510b8e35bdf98986338c59f80386cf
viral666com <45723658+viral666com@users.noreply.github.com>
2018-12-10 12:04:32 +0700

    Update safekelink.js

--- safekelink.js
+++ safekelink.js
@@ -131,7 +131,7 @@ function auto_safeconvert() {
         }
         if (a_to_vh == false) {
             var encryptedUrl = Base64.encode(a_to_vi);
-            a_to_ve[i].href = "https://viral666.com/safekelink/#.XAxchh-ySM8.html?url=" + encryptedUrl;
+            a_to_ve[i].href = "https://viral666.com/safekelink/#.XAxchh-ySM8.xml?url=" + encryptedUrl;
             a_to_ve[i].rel = "nofollow";
             a_to_vb++;
             a_to_vc += i + ":::" + a_to_ve[i].href + "\n";
